138b3a1c620e1b8b526f22299a772ee8e2f28ac3
Igor Suvorov <hi@isuvorov.com>
2023-07-18 19:15:26 +0300

    fix: ci

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.all('*', (req, res) => {
-  res.send('Hello World! v2')
+  res.send('Hello API World!')
 })
 
 app.listen(port, () => {
